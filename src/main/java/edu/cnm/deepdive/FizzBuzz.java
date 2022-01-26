de0e6a7e720da0390cbf3e69e288e7973755957a
seanblann <sblann@cnm.edu>
2022-01-26 13:54:45 -0700

    Implemented main with limit set by cmd.

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -5,6 +5,15 @@ public class FizzBuzz {
   public static final String FIZZ = "Fizz";
   public static final String BUZZ = "Buzz";
   public static final String FIZZ_BUZZ = "FizzBuzz";
+  private static final int DEFAULT_LIMIT = 100;
+
+  public static void main(String[] args) {
+    int limit = (args.length > 0) ? Integer.parseInt(args [0]) : DEFAULT_LIMIT;
+    FizzBuzz fb = new FizzBuzz();
+    for (int i = 1; i <= limit; i++) {
+      System.out.println(fb.check(i));
+    }
+  }
 
   public String check(int value) {
     String result;
